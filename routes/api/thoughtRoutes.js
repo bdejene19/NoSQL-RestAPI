bb469c44a390a7d743ee4db45bd455b7f88f7105
Bemnet Dejene <bdejene19@gmail.com>
2022-03-19 00:39:02 -0400

    reworked routes to integrate reaction routes into thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,9 +4,15 @@ const {
   getAllThoughts,
 } = require("../../controllers/thoughtController");
 
+const {
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/reactionController");
+
 const thought = require("express").Router();
 
 thought.route("/").get(getAllThoughts).post(createThought);
 thought.route("/:id").get(findSpecificThought);
+thought.route("/:id/reactions").post(createReaction).delete(deleteReaction);
 
 module.exports = thought;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,9 +5,12 @@ const {
   getUserById,
   updateUserById,
   deleteUser,
+  addFriend,
+  deleteFriend,
 } = require("../../controllers/userController");
 
 user.route("/:id").get(getUserById).delete(deleteUser).put(updateUserById);
 user.route("/").get(getAllUsers).post(createUser);
+user.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = user;
